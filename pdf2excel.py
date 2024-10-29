85ee634544b6bc8f62b9a62cbd74eb2b69c75a9d
Merholg <ixibitus@gmail.com>
2024-10-29 16:12:14 +0300

    Add main entry point

--- pdf2excel.py
+++ pdf2excel.py
@@ -1,9 +1,41 @@
 #!/usr/bin/env python3
 
+import openpyxl
+import sys
 import pdfplumber
 import pandas as pd
 
+
+def pdf_to_excel(pdf_file, excel_file):
+    with pdfplumber.open(pdf_file) as pdf:
+        all_tables = []
+        for page in pdf.pages:
+            tables = page.extract_tables()
+            for table in tables:
+                if table:
+                    df = pd.DataFrame(table)
+                    all_tables.append(df)
+
+        if not all_tables:
+            all_tables.append(pd.DataFrame([["No tables found"]]))
+
+        with pd.ExcelWriter(excel_file, engine='openpyxl') as writer:
+            for idx, df in enumerate(all_tables):
+                df.to_excel(writer, sheet_name=f'Sheet{idx}', index=False)
+
+
 if __name__ == '__main__':
     """
+    Чтение таблиц из PDF файла и их запись в файл XLSX
+    Usage: pdf2excel.py file.pdf file.xlsx
     """
-    pass
+    if len(sys.argv) > 2:
+        try:
+            pdf_to_excel(sys.argv[1], sys.argv[2])
+            sys.exit(0)
+        except Exception as error:
+            print(f"Unexpected error: {error}")
+            sys.exit(1)
+    else:
+        print("Usage: pdf2excel.py file.pdf file.xlsx")
+        sys.exit(1)
